d3e3f4b5b866d9a14a3a593aa395e42447eb7524
李俊平 <2544054976@qq.com>
2022-04-18 11:01:47 +0800

    任务修改页面

--- src/pages/spiderTask/components/UpdateForm.tsx
+++ src/pages/spiderTask/components/UpdateForm.tsx
@@ -1,8 +1,8 @@
-import React, {useCallback, useEffect, useState} from 'react';
-import {Form, Button, Input, Modal, Row, Col, Select, message} from 'antd';
-import CronModal from "@/components/CronModel";
-import {validateCronExpress} from "@/services/open-crawler/spidertask";
-import {querySpiderList} from "@/services/open-crawler/spider";
+import React, { useCallback, useEffect, useState } from 'react';
+import { Form, Button, Input, Modal, Row, Col, Select, message } from 'antd';
+import CronModal from '@/components/CronModel';
+import { validateCronExpress } from '@/services/open-crawler/spidertask';
+import { querySpiderList } from '@/services/open-crawler/spider';
 
 export interface UpdateFormProps {
   onCancel: (flag?: boolean, formVals?: Partial<API.SpiderTask>) => void;
@@ -30,35 +30,36 @@ const UpdateForm: React.FC<UpdateFormProps> = (props) => {
   const [cronModalVisible, handleCronModalVisible] = useState<boolean>(false);
   const [cronExpressValue, setCronExpressValue] = useState<any>(values.cronExpression);
   const [spiderList, setSpiderList] = useState<API.Spider[]>([]);
-  const [spiderId, setSpiderId] = useState<number>();
+  const [spiderId, setSpiderId] = useState<any>(values.spiderId);
 
   const handleChange = (value: any) => {
     setSpiderId(value);
   };
 
   const onFetchSpiderData = useCallback(async () => {
-    const result = await querySpiderList();
+    const result = await querySpiderList(values.spiderId);
     setSpiderList(result);
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     onFetchSpiderData().then();
-  },[]);
+  }, []);
 
   const handleSave = async () => {
     const fieldsValue: any = await form.validateFields();
-    if(!cronExpressValue || cronExpressValue.length === 0){
-      message.error("cron 表达式不能为空");
+    if (!cronExpressValue || cronExpressValue.length === 0) {
+      message.error('cron 表达式不能为空');
       return;
     }
     const result = await validateCronExpress(cronExpressValue);
-    if(!result || result !== 'success'){
-      message.error("cron 校验失败，请重新输入");
+    if (!result || result !== 'success') {
+      message.error('cron 校验失败，请重新输入');
       return;
     }
     handleUpdate({
       ...values,
       ...fieldsValue,
+      cronExpression: cronExpressValue,
     });
   };
 
@@ -75,7 +76,7 @@ const UpdateForm: React.FC<UpdateFormProps> = (props) => {
 
   return (
     <Modal
-      width={640}
+      width={900}
       bodyStyle={{ padding: '32px 40px 48px' }}
       destroyOnClose
       title="编辑调度任务"
@@ -88,6 +89,8 @@ const UpdateForm: React.FC<UpdateFormProps> = (props) => {
         form={form}
         initialValues={{
           id: values.id,
+          taskName: values.taskName,
+          handlerName: values.handlerName,
           cronExpression: values.cronExpression,
         }}
       >
@@ -113,29 +116,30 @@ const UpdateForm: React.FC<UpdateFormProps> = (props) => {
         </Row>
         <Row>
           <Col span={12}>
-            <FormItem
-              name="spiderId"
-              label="选择爬虫"
-            >
+            <FormItem name="spiderId" label="选择爬虫">
               <Select
                 value={spiderId}
+                defaultValue={spiderId}
                 placeholder="请选项爬虫模板"
                 defaultActiveFirstOption={true}
                 onChange={handleChange}
               >
-                {spiderList.map(d => (
-                  <Option key={d.id} value={d.id}>{d.name}</Option>
+                {spiderList.map((d) => (
+                  <Option key={d.id} value={d.id}>
+                    {d.name}
+                  </Option>
                 ))}
               </Select>
             </FormItem>
           </Col>
           <Col span={12}>
-            <FormItem
-              name="cronExpression"
-              label="Cron 表达式"
-            >
-              <Input.Group compact style={{display: 'flex'}}>
-                <Input placeholder="请输入Cron 表达式"  value={cronExpressValue} onChange={(e)=>setCronExpressValue(e.target.value)}/>
+            <FormItem name="cronExpression" label="Cron 表达式">
+              <Input.Group compact style={{ display: 'flex' }}>
+                <Input
+                  placeholder="请输入Cron 表达式"
+                  value={cronExpressValue}
+                  onChange={(e) => setCronExpressValue(e.target.value)}
+                />
                 <Button
                   type="primary"
                   onClick={() => {
@@ -152,7 +156,7 @@ const UpdateForm: React.FC<UpdateFormProps> = (props) => {
         <CronModal
           modalVisible={cronModalVisible}
           onCancel={() => handleCronModalVisible(false)}
-          onSubmit={(value)=>{
+          onSubmit={(value) => {
             setCronExpressValue(value);
             handleCronModalVisible(false);
           }}
--- src/services/open-crawler/spider.ts
+++ src/services/open-crawler/spider.ts
@@ -1,14 +1,12 @@
 import { request } from 'umi';
 
-export async function fetchSpiderPage(
-  params: {
-    // query
-    /** 当前的页码 */
-    current?: number;
-    /** 页面的容量 */
-    pageSize?: number;
-  }
-) {
+export async function fetchSpiderPage(params: {
+  // query
+  /** 当前的页码 */
+  current?: number;
+  /** 页面的容量 */
+  pageSize?: number;
+}) {
   return request('/spider/page', {
     method: 'GET',
     params: {
@@ -20,26 +18,29 @@ export async function fetchSpiderPage(
 export async function updateSpider(params: Partial<API.Spider>) {
   return request('/spider/update', {
     method: 'PUT',
-    data: {...params}
+    data: { ...params },
   });
 }
 
 export async function addSpider(params: Partial<API.Spider>) {
   return request('/spider/save', {
     method: 'POST',
-    data: {...params}
+    data: { ...params },
   });
 }
 
-export async function removeSpider(params: {ids: number[]}) {
+export async function removeSpider(params: { ids: number[] }) {
   return request('/spider/delete', {
     method: 'DELETE',
-    data: {...params}
+    data: { ...params },
   });
 }
 
-export async function querySpiderList() {
+export async function querySpiderList(spiderId?: any) {
   return request(`/spider/list`, {
     method: 'GET',
+    params: {
+      spiderId: spiderId ? spiderId : '',
+    },
   });
 }
